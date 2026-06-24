d614ca60ef314376aeeceed90bc3b71bb2474775
xiao-1997 <904592524@qq.com>
2026-06-24 23:50:45 +0800

    fix: UIKit lifecycle, iOS 13 target for compatibility

--- HOKAuto/HOKAutoApp.swift
+++ HOKAuto/HOKAutoApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
+import UIKit
 
 @main
-struct HOKAutoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let contentView = ContentView()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+        return true
     }
 }
